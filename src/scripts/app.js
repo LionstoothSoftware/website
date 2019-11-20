a9ccda56009d96350e45dc81eca3cb2a2dad6fac
erturne <eric@ericturner.dev>
2019-11-19 19:10:30 -0500

    Add eslint and prettier

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,5 @@
-import {MDCTopAppBar} from '@material/top-app-bar/index';
-import {MDCList} from '@material/list/index';
+import { MDCTopAppBar } from '@material/top-app-bar/index';
+import { MDCList } from '@material/list/index';
 
 const topAppBarElement = document.querySelector('.lts-top-app-bar');
 MDCTopAppBar.attachTo(topAppBarElement);
@@ -14,14 +14,14 @@ registerAnimations();
  * Registers an event handler on the titles of each of the steps of the startup-creation process.
  */
 function registerListTitleInteractions() {
-    let eventType = 'click';
-    if (document.ontouchstart) {
-        eventType = 'touchstart';
-    }
-    const titles = document.querySelectorAll('.lts-process-step-title');
-    for (const title of titles) {
-        title.addEventListener(eventType, handleListTitleInteraction, false);
-    }
+   let eventType = 'click';
+   if (document.ontouchstart) {
+      eventType = 'touchstart';
+   }
+   const titles = document.querySelectorAll('.lts-process-step-title');
+   for (const title of titles) {
+      title.addEventListener(eventType, handleListTitleInteraction, false);
+   }
 }
 
 /**
@@ -30,25 +30,25 @@ function registerListTitleInteractions() {
  * @returns {boolean}
  */
 function handleListTitleInteraction(ev) {
-    if (ev) {
-        // Find the title element that is a parent of the element that the user interacted with.
-        const title = ev.target.closest('.lts-process-step-title');
-        if (title) {
-            // Adding or removing the lts-collapse class controls whether or not the details of
-            // the process step is visible or not (through CSS).
-            title.classList.toggle('lts-collapse');
+   if (ev) {
+      // Find the title element that is a parent of the element that the user interacted with.
+      const title = ev.target.closest('.lts-process-step-title');
+      if (title) {
+         // Adding or removing the lts-collapse class controls whether or not the details of
+         // the process step is visible or not (through CSS).
+         title.classList.toggle('lts-collapse');
 
-            updateListTitleExpandCollapseIcon(title);
-        } else {
-            console.error('No title element found');
-        }
+         updateListTitleExpandCollapseIcon(title);
+      } else {
+         console.error('No title element found');
+      }
 
-        // Prevent touch-enabled browsers from firing a mouse event 300ms after a touch event.
-        ev.preventDefault();
-    } else {
-        console.error('No title interaction event to process');
-    }
-    return false;
+      // Prevent touch-enabled browsers from firing a mouse event 300ms after a touch event.
+      ev.preventDefault();
+   } else {
+      console.error('No title interaction event to process');
+   }
+   return false;
 }
 
 /**
@@ -58,59 +58,59 @@ function handleListTitleInteraction(ev) {
  * @param titleElement
  */
 function updateListTitleExpandCollapseIcon(titleElement) {
-    if (titleElement) {
-        const angleIcon = titleElement.querySelector('.lts-angle-icon');
-        if (angleIcon) {
-            if (titleElement.classList.contains('lts-collapse')) {
-                angleIcon.classList.remove('la-angle-up');
-                angleIcon.classList.add('la-angle-down');
-            } else {
-                angleIcon.classList.remove('la-angle-down');
-                angleIcon.classList.add('la-angle-up');
-            }
-        } else {
-            console.error('No angle icon element found');
-        }
-    } else {
-        console.error('No title element found');
-    }
+   if (titleElement) {
+      const angleIcon = titleElement.querySelector('.lts-angle-icon');
+      if (angleIcon) {
+         if (titleElement.classList.contains('lts-collapse')) {
+            angleIcon.classList.remove('la-angle-up');
+            angleIcon.classList.add('la-angle-down');
+         } else {
+            angleIcon.classList.remove('la-angle-down');
+            angleIcon.classList.add('la-angle-up');
+         }
+      } else {
+         console.error('No angle icon element found');
+      }
+   } else {
+      console.error('No title element found');
+   }
 }
 
 /**
  * Sets up events for animation.
  */
 function registerAnimations() {
-    const stepIcons = document.querySelectorAll('.lts-icon--step');
-    if (stepIcons) {
-        for (const stepIcon of stepIcons) {
-            stepIcon.addEventListener('mouseover', triggerAnimation, false);
-        }
-    } else {
-        console.error('No elements were found to animate');
-    }
+   const stepIcons = document.querySelectorAll('.lts-icon--step');
+   if (stepIcons) {
+      for (const stepIcon of stepIcons) {
+         stepIcon.addEventListener('mouseover', triggerAnimation, false);
+      }
+   } else {
+      console.error('No elements were found to animate');
+   }
 }
 
 function triggerAnimation(ev) {
-    if (ev) {
-        animateCSS(ev.target, 'flip');
-    } else {
-        console.error('No animation event to handle')
-    }
+   if (ev) {
+      animateCSS(ev.target, 'flip');
+   } else {
+      console.error('No animation event to handle');
+   }
 }
 
 function animateCSS(node, animationName, callback) {
-    if (node) {
-        // I want the animation to happen once each time, so when the animation ends remove the animated class
-        // from the element.
-        function handleAnimationEnd() {
-            node.classList.remove('animated', animationName);
-            node.removeEventListener('animationend', handleAnimationEnd);
-            if (typeof callback === 'function') callback()
-        }
+   if (node) {
+      // I want the animation to happen once each time, so when the animation ends remove the animated class
+      // from the element.
+      const handleAnimationEnd = function handleAnimationEnd() {
+         node.classList.remove('animated', animationName);
+         node.removeEventListener('animationend', handleAnimationEnd);
+         if (typeof callback === 'function') callback();
+      };
 
-        node.addEventListener('animationend', handleAnimationEnd);
-        node.classList.add('animated', animationName);
-    } else {
-        console.error('No element to animate')
-    }
+      node.addEventListener('animationend', handleAnimationEnd);
+      node.classList.add('animated', animationName);
+   } else {
+      console.error('No element to animate');
+   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,83 +3,77 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const htmlWebpackPlugins = [
-    {template: './src/index.pug', filename: 'index.html'},
-    {template: './src/contact.pug', filename: 'contact.html'},
+   { template: './src/index.pug', filename: 'index.html' },
+   { template: './src/contact.pug', filename: 'contact.html' },
 ].map(plugin => new HtmlWebpackPlugin(plugin));
 
 module.exports = {
-    entry: [
-        './src/styles/app.scss',
-        './src/styles/animate.min.css',
-        './src/scripts/app.js',
-        './src/fonts/line-awesome/css/line-awesome.min.css'],
-    plugins: htmlWebpackPlugins,
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                ]
+   entry: [
+      './src/styles/app.scss',
+      './src/styles/animate.min.css',
+      './src/scripts/app.js',
+      './src/fonts/line-awesome/css/line-awesome.min.css',
+   ],
+   plugins: htmlWebpackPlugins,
+   module: {
+      rules: [
+         {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+         },
+         {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader'],
+         },
+         {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            query: {
+               presets: ['@babel/preset-env'],
             },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader',
-                ],
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['@babel/preset-env'],
-                },
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader',
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: 'bundle.css',
-                        },
-                    },
-                    {loader: 'extract-loader'},
-                    {loader: 'css-loader'},
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: () => [autoprefixer()]
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sassOptions: {
-                                includePaths: ['./node_modules'],
-                            }
-                        },
-                    }
-                ],
-            },
-            {
-                test: /\.pug$/,
-                use: [
-                    {
-                        loader: 'pug-loader',
-                    }
-                ]
-            },
-        ],
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-    }
+         },
+         {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: ['file-loader'],
+         },
+         {
+            test: /\.scss$/,
+            use: [
+               {
+                  loader: 'file-loader',
+                  options: {
+                     name: 'bundle.css',
+                  },
+               },
+               { loader: 'extract-loader' },
+               { loader: 'css-loader' },
+               {
+                  loader: 'postcss-loader',
+                  options: {
+                     plugins: () => [autoprefixer()],
+                  },
+               },
+               {
+                  loader: 'sass-loader',
+                  options: {
+                     sassOptions: {
+                        includePaths: ['./node_modules'],
+                     },
+                  },
+               },
+            ],
+         },
+         {
+            test: /\.pug$/,
+            use: [
+               {
+                  loader: 'pug-loader',
+               },
+            ],
+         },
+      ],
+   },
+   devServer: {
+      contentBase: path.join(__dirname, 'public'),
+   },
 };
