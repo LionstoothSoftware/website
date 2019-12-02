ac5e9380ecc0ab1648e5b71e153016309a46fa1e
Eric Turner <codeoftheprogrammer@gmail.com>
2019-12-01 20:48:44 -0500

    Fix babel preset-env usage

--- webpack.common.js
+++ webpack.common.js
@@ -33,10 +33,12 @@ module.exports = {
             use: ['file-loader'],
          },
          {
-            test: /\.js$/,
-            loader: 'babel-loader',
-            query: {
-               presets: ['@babel/preset-env'],
+            test: /\.m?js$/,
+            use: {
+               loader: 'babel-loader',
+               options: {
+                  presets: ['@babel/preset-env'],
+               },
             },
          },
          {
